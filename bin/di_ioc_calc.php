69b252ad6d718cdc05da1339a5d80b601710f6c0
ArtRyzen <artembabichev@gmail.com>
2023-09-29 19:38:43 +0300

    di and ioc

--- bin/di_ioc_calc.php
+++ bin/di_ioc_calc.php
@@ -1,15 +1,26 @@
 <?php
 
-
-interface CalculatorInterface
+/// Interfaces
+interface ILoggerInterface
 {
+    public function log($message);
+}
 
+interface INotifierInterface
+{
+    public function send($message);
+}
+
+interface ICalculatorInterface
+{
     public function add($a, $b);
 
     public function multiply($a, $b);
 }
 
-class SimpleCalculator implements CalculatorInterface
+
+/// Classes
+class IntICalculator implements ICalculatorInterface
 {
     public function add($a, $b): int
     {
@@ -22,20 +33,42 @@ class SimpleCalculator implements CalculatorInterface
     }
 }
 
-interface NotifierInterface
+class FloatICalculator implements ICalculatorInterface
 {
-    public function send($message);
-}
-
-class CommandLineNotifier implements NotifierInterface
-{
-    public function send($message)
+    public function add($a, $b): float
     {
-        echo "Command Line: $message\n";
+        return $a + $b;
+    }
+
+    public function multiply($a, $b): float
+    {
+        return $a * $b;
     }
 }
 
-class TelegramNotifier implements NotifierInterface
+class FileILogger implements ILoggerInterface
+{
+
+    public function __construct(
+        protected string $filePath,
+    ) {
+    }
+
+    public function log($message): void
+    {
+        file_put_contents($this->filePath, $message . PHP_EOL, FILE_APPEND);
+    }
+}
+
+class CliINotifier implements INotifierInterface
+{
+    public function send($message): void
+    {
+        echo $message . PHP_EOL;
+    }
+}
+
+class TelegramINotifier implements INotifierInterface
 {
 
     public function __construct(
@@ -44,67 +77,212 @@ class TelegramNotifier implements NotifierInterface
     ) {
     }
 
-    public function send($message)
+    public function send($message): void
     {
 
         echo "Message sent to Telegram: $message\n";
     }
 }
 
+/// Container builder
 class IoCContainer
 {
-    private $bindings = [];
+    // Масив для зберігання прив'язок
+    private array $bindings = [];
 
-    public function bind($abstract, $concrete)
+    // Метод для реєстрації прив'язки у контейнері
+    public function bind($abstract, $concrete): void
     {
         $this->bindings[$abstract] = $concrete;
     }
 
+    // Метод для створення об'єкта на основі його абстракції
     public function make($abstract)
     {
         return $this->bindings[$abstract]();
     }
 }
 
-// Создание IoC контейнера
+/* ENUM  */
+
+enum ECalcOperations: string
+{
+
+    public const  ADD = 'add';
+    public const MULTIPLY = 'multiply';
+    public const MULTI = 'multi';
+
+    ///CalcOperations::cases() не працює з константою.
+    public const TO_STR = 'add, multiply, multi';
+
+}
+
+/* Processor  */
+
+interface InputInterface
+{
+    public function handle(array $args);
+}
+
+class CliCommandHandler implements InputInterface
+{
+    public function __construct(
+        protected CalculatorProcessor $processor,
+    ) {
+    }
+
+    public function handle(array $args)
+    {
+        if (count($args) < 4) {
+            die("Usage: {$args[0]} <operation> <number1> <number2>" . PHP_EOL);
+        }
+
+        $operation = $args[1];
+        $number1 = $args[2];
+        $number2 = $args[3];
+
+        return $this->processor->calculate($operation, $number1, $number2);
+    }
+}
+
+class InteractiveCommandHandler implements InputInterface
+{
+    public function __construct(
+        protected CalculatorProcessor $processor,
+    ) {
+    }
+
+    public function handle(array $args = [])
+    {
+        $operation = $this->prompt("Введіть операцію (" . ECalcOperations::TO_STR . "): ");
+        $number1 = $this->prompt('Введіть перше число: ');
+        $number2 = $this->prompt('Введіть друге число: ');
+
+        return $this->processor->calculate($operation, $number1, $number2);
+    }
+
+    protected function prompt($message): string
+    {
+        echo $message;
+        return trim(fgets(STDIN));
+    }
+}
+
+class CalculatorProcessor
+{
+    protected ICalculatorInterface $calculator;
+    protected INotifierInterface $notifier;
+    protected ILoggerInterface $logger;
+
+    public function __construct(
+        ICalculatorInterface $calculator,
+        INotifierInterface   $notifier,
+        ILoggerInterface     $logger
+    ) {
+        $this->calculator = $calculator;
+        $this->notifier = $notifier;
+        $this->logger = $logger;
+    }
+
+    public function calculate(string $operation, $number1, $number2)
+    {
+        $calculator = [
+            ECalcOperations::ADD => fn() => $this->calculator->add($number1, $number2),
+            // Дублюємо помноження, для простих юзерів
+            ECalcOperations::MULTIPLY => fn() => $this->calculator->multiply($number1, $number2),
+            ECalcOperations::MULTI => fn() => $this->calculator->multiply($number1, $number2),
+        ];
+
+        $matchResult = $calculator[$operation] ?? die("Unknown operation: $operation. Supported operations: " . ECalcOperations::TO_STR . PHP_EOL);
+
+        $result = $matchResult();
+        $this->logger->log("Operation: {$operation}, Number1: {$number1}, Number2: {$number2}, Result: {$result}");
+        $this->notifier->send("Result of {$operation}: {$result}");
+
+        return $result;
+    }
+}
+
+/* Створення IoC контейнера  */
+
 $container = new IoCContainer();
 
-// Регистрация зависимостей
+// Контракти
 $container->bind(
-    'CalculatorInterface', function () {
-    return new SimpleCalculator();
+    'IntCalculatorContract', function () {
+    return new IntICalculator();
 }
 );
 
 $container->bind(
-    'NotifierInterface', function () {
-    //return new CommandLineNotifier();
-    return new TelegramNotifier('SomeToken123', 'someChatId123');
-
+    'FloatCalculatorContract', function () {
+    return new FloatICalculator();
 }
 );
 
-
-if ($argc < 4) {
-    die("Usage: {$argv[0]} <operation> <number1> <number2>\n");
+$container->bind(
+    'CliNotifierContract', function () {
+    return new CliINotifier();
 }
+);
 
-$operation = $argv[1];
-$number1 = (float)$argv[2];
-$number2 = (float)$argv[3];
-
-$calculator = $container->make('CalculatorInterface');
-
-switch ($operation) {
-    case 'add':
-        $result = $calculator->add($number1, $number2);
-        break;
-    case 'multiply':
-        $result = $calculator->multiply($number1, $number2);
-        break;
-    default:
-        die("Unknown operation: $operation. Supported operations: add, multiply.\n");
+$container->bind(
+    'TelegramNotifierContract', function () {
+    return new TelegramINotifier('SomeToken123', 'someChatId123');
 }
+);
 
-$notifier = $container->make('NotifierInterface');
-$notifier->send("Result of {$operation}: $result");
+$container->bind(
+    'TestLoggerContract', function () {
+    return new FileILogger('test_logfile.log');
+}
+);
+
+$container->bind(
+    'ProdLoggerContract', function () {
+    return new FileILogger('prod_logfile.log');
+}
+);
+
+/* Обробники */
+$container->bind(
+    'FloatCalcCLIProcessor', function () use ($container) {
+    return new CliCommandHandler(
+        new CalculatorProcessor(
+            $container->make('FloatCalculatorContract'),
+            $container->make('CliNotifierContract'),
+            $container->make('TestLoggerContract')
+        )
+    );
+}
+);
+
+$container->bind(
+    'IntCalcCLIProcessor', function () use ($container) {
+    return new CliCommandHandler(
+        new CalculatorProcessor(
+            $container->make('IntCalculatorContract'),
+            $container->make('CliNotifierContract'),
+            $container->make('TestLoggerContract')
+        )
+    );
+}
+);
+
+$container->bind(
+    'FloatCalcInteractiveProcessor', function () use ($container) {
+    return new InteractiveCommandHandler(
+        new CalculatorProcessor(
+            $container->make('FloatCalculatorContract'),
+            $container->make('TelegramNotifierContract'),
+            $container->make('ProdLoggerContract')
+        )
+    );
+}
+);
+
+/* Реалізація */
+$processor = $container->make('FloatCalcInteractiveProcessor');
+$processor->handle($argv);
+
+exit();
