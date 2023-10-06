c61a470aa272f1488f96bea256e24d108016c78f
ArtRyzen <artembabichev@gmail.com>
2023-10-06 06:38:35 +0300

    mysql DZ done

--- docker/databases/mysql_dump/2023_10_06_dz_modification.sql
+++ docker/databases/mysql_dump/2023_10_06_dz_modification.sql
@@ -1,30 +1,32 @@
--- Создаем таблицу для регионов
+USE base;
+
+-- Створюємо таблицю для регіонів
 CREATE TABLE regions
 (
     region_id      INT AUTO_INCREMENT PRIMARY KEY,
-    region_name_uk VARCHAR(255) NOT NULL UNIQUE,
-    region_name_ru VARCHAR(255) NOT NULL UNIQUE
+    region_name_uk VARCHAR(142) NOT NULL UNIQUE,
+    region_name_ru VARCHAR(142) NOT NULL UNIQUE
 );
 
--- Вставка уникальных регионов из исходной таблицы
+-- Вставка унікальних регіонів з початкової таблиці
 INSERT INTO regions (region_name_uk, region_name_ru)
 SELECT DISTINCT region_name_uk, region_name_ru
 FROM catalog_city;
 
--- Создаем таблицу для уникальных районов
+-- Створюємо таблицю для унікальних районів
 CREATE TABLE districts
 (
     district_id      INT AUTO_INCREMENT PRIMARY KEY,
-    district_name_uk VARCHAR(255) NOT NULL UNIQUE,
-    district_name_ru VARCHAR(255) NOT NULL UNIQUE
+    district_name_uk VARCHAR(142) NOT NULL UNIQUE,
+    district_name_ru VARCHAR(142) NOT NULL UNIQUE
 );
 
--- Вставка уникальных районов из исходной таблицы
+-- Вставка унікальних районів з початкової таблиці
 INSERT INTO districts (district_name_uk, district_name_ru)
 SELECT DISTINCT area_name_uk, area_name_ru
 FROM catalog_city;
 
--- Создаем связующую таблицу между областями и районами
+-- Створюємо зв'язуючу таблицю між областями та районами
 CREATE TABLE region_district
 (
     region_district_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -35,7 +37,7 @@ CREATE TABLE region_district
     UNIQUE (region_id, district_id)
 );
 
--- Вставка уникальных комбинаций область-район из исходной таблицы
+-- Вставка унікальних комбінацій область-район з початкової таблиці
 INSERT INTO region_district (region_id, district_id)
 SELECT r.region_id, d.district_id
 FROM catalog_city c
@@ -43,39 +45,40 @@ FROM catalog_city c
          JOIN districts d ON c.area_name_uk = d.district_name_uk AND c.area_name_ru = d.district_name_ru
 GROUP BY r.region_id, d.district_id;
 
--- Создаем таблицу для названий городов
+-- Створюємо таблицю для назв міст
 CREATE TABLE city_names
 (
     city_name_id INT AUTO_INCREMENT PRIMARY KEY,
-    name_uk      VARCHAR(255) NOT NULL UNIQUE,
-    name_ru      VARCHAR(255) NOT NULL UNIQUE,
-    slug         VARCHAR(255) NOT NULL UNIQUE
+    name_uk      VARCHAR(142) NOT NULL UNIQUE,
+    name_ru      VARCHAR(142) NOT NULL UNIQUE,
+    slug         VARCHAR(71) NOT NULL UNIQUE
 );
 
--- Вставка уникальных названий городов из исходной таблицы
+-- Вставка унікальних назв міст з початкової таблиці
 INSERT INTO city_names (name_uk, name_ru, slug)
 SELECT DISTINCT name_uk, name_ru, slug
 FROM catalog_city;
 
--- Создаем таблицу для городов
+-- Створюємо таблицю для міст
 CREATE TABLE catalog_new
 (
-    city_id            INT AUTO_INCREMENT PRIMARY KEY,
-    region_district_id INT,
-    city_name_id       INT,
-    new_post_city_id   VARCHAR(255) NULL,
-    justin_city_id     INT NULL,
-    ukr_poshta_city_id INT NULL,
-    created_at         TIMESTAMP NOT NULL,
-    updated_at         TIMESTAMP NOT NULL,
+    id                  INT NOT NULL PRIMARY KEY,
+    region_district_id  INT,
+    city_name_id        INT,
+    new_post_city_id    VARCHAR(77) NULL,
+    justin_city_id      INT NULL,
+    ukr_poshta_city_id  INT NULL,
+    created_at          TIMESTAMP NOT NULL,
+    updated_at          TIMESTAMP NOT NULL,
     FOREIGN KEY (region_district_id) REFERENCES region_district (region_district_id),
     FOREIGN KEY (city_name_id) REFERENCES city_names (city_name_id)
 );
 
--- Вставка данных городов из исходной таблицы
-INSERT INTO catalog_new (city_name_id, new_post_city_id, justin_city_id, ukr_poshta_city_id, region_district_id,
+-- Вставка даних міст з початкової таблиці
+INSERT INTO catalog_new (id, city_name_id, new_post_city_id, justin_city_id, ukr_poshta_city_id, region_district_id,
                          created_at, updated_at)
-SELECT cn.city_name_id,
+SELECT c.id,
+       cn.city_name_id,
        c.new_post_city_id,
        c.justin_city_id,
        c.ukr_poshta_city_id,
@@ -87,3 +90,9 @@ FROM catalog_city c
          JOIN regions r ON c.region_name_uk = r.region_name_uk AND c.region_name_ru = r.region_name_ru
          JOIN districts d ON c.area_name_uk = d.district_name_uk AND c.area_name_ru = d.district_name_ru
          JOIN region_district rd ON r.region_id = rd.region_id AND d.district_id = rd.district_id;
+
+-- Видаляємо початкову таблицю
+DROP TABLE catalog_city;
+
+-- Перейменовуємо нову таблицю
+ALTER TABLE catalog_new RENAME TO catalog_city;
